2c7ac3cd8429316e47696d2d11d272fdec553e4a
Ayahassan94 <ayah7386@gmail.com>
2021-01-06 23:29:51 +0200

    about

---
+++ about/about.js
@@ -0,0 +1,8 @@
+/*$(".names").animate({top: '+=900px'},2000,function(){
+    $(this).css('color','red')
+ })
+
+*/
+$(".names").animate({top: '-=300px' ,height: '200px', width:'400px'},3000,function(){
+    $(this).css('color','pink')
+ })
