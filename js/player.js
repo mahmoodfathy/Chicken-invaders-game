e50d0f7b249f8afd70135d6ce2fa63bf9be89551
rjOwl <mostafayossef100@yahoo.com>
2021-01-13 01:21:35 +0200

    Adding update position and checkBoundries

--- js/player.js
+++ js/player.js
@@ -1,6 +1,4 @@
 class Player{
-    constructor(gameObject){
-    }
     create(container, img){
         game.playerX = game.width / 2;
         game.playerY = game.height - 50;
@@ -9,5 +7,34 @@ class Player{
         player.className = "player";
         container.appendChild(player);
         game.setPosition(player, game.playerX, game.playerY);
+      }
+      update(dt, container) {
+        if (game.leftPressed)
+            game.playerX -= dt * game.PLAYER_MAX_SPEED;
+        if (game.rightPressed)
+          game.playerX += dt * PLAYER_MAX_SPEED;
+
+        // not to get out of the screen
+        game.playerX = this.checkBoundries(
+          game.playerX,
+          PLAYER_WIDTH,
+          GAME_WIDTH - PLAYER_WIDTH
+        );
+
+        if (game.spacePressed && game.playerCooldown <= 0) {
+          laser.createLaser($container, game.playerX, game.playerY);
+          game.playerCooldown = LASER_COOLDOWN;
+        }
+        if (game.playerCooldown > 0) {
+          game.playerCooldown -= dt;
+        }
+      
+        const player = document.querySelector(".player");
+        setPosition(player, game.playerX, game.playerY);
+      }
+
+      // here just checking if te player reached the boundries of the screen
+      checkBoundries(playerX, min, max) {
+          return playerX < min? min : (playerX > max ? max : playerX) 
       }
 }
