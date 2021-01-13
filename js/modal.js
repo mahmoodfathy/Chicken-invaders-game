99009df0d58a283792be077f8097941df913c43e
rjOwl <mostafayossef100@yahoo.com>
2021-01-13 05:27:32 +0200

    Toggle modal on Esc

--- js/modal.js
+++ js/modal.js
@@ -1,19 +1,8 @@
-var editValueButton = document.querySelector('button');
 var demoContainer = document.querySelector('.demo-container');
 var backdrop;
 var modal;
 
-var outputParagraph = document.querySelector('#quote');
-// var textEdit = document.querySelector('.modal textarea');
-
-var quote = 'A Modal Component on a Webpage - how hard could it be?';
-var editedQuote = '';
-
-function updateParagraph() {
-  outputParagraph.textContent = quote;
-}
-
-function closeModal() {
+function closeModal(){
   if (backdrop) {
     backdrop.remove();
   }
@@ -22,61 +11,58 @@ function closeModal() {
     modal.remove();
   }
 }
+clicked = false;
 
-updateParagraph();
-
-editValueButton.addEventListener('click', function() {
-  backdrop = document.createElement('div');
-  backdrop.classList.add('backdrop');
-  backdrop.addEventListener('click', closeModal);
-  document.body.insertBefore(backdrop, demoContainer);
-  backdrop.addEventListener('click', closeModal);
-
-  modal = document.createElement('div');
-  modal.classList.add('modal');
-
-  var modalHeading = document.createElement('h1');
-  modalHeading.textContent = 'Edit your Statement';
-  modal.appendChild(modalHeading);
-
-  var textEditContainer = document.createElement('div');
-  textEditContainer.classList.add('modal-input');
-  modal.appendChild(textEditContainer);
-
-  var textEditArea = document.createElement('textarea');
-  textEditArea.rows = '3';
-  textEditArea.addEventListener('input', function() {
-    editedQuote = textEditArea.value;
-  });
-  textEditArea.value = quote;
-  textEditContainer.appendChild(textEditArea);
-
-  var modalActionsContainer = document.createElement('div');
-  modalActionsContainer.classList.add('modal-actions');
-  modal.appendChild(modalActionsContainer);
-
-  var cancelButton = document.createElement('button');
-  cancelButton.setAttribute('type', 'button');
-  cancelButton.classList.add('btn-cancel');
-  cancelButton.textContent = 'Cancel';
-  cancelButton.addEventListener('click', closeModal);
-  modalActionsContainer.appendChild(cancelButton);
-
-  var confirmButton = document.createElement('button');
-  confirmButton.setAttribute('type', 'button');
-  confirmButton.classList.add('btn-confirm');
-  confirmButton.textContent = 'Confirm';
-  confirmButton.addEventListener('click', function() {
-    closeModal();
-    if (editedQuote.trim().length > 0) {
-      quote = editedQuote;
-      updateParagraph();
+document.addEventListener('keydown', function(e) {
+  if(e.key == "Escape" && clicked){
+    console.log("TEST");
+    closeModal()
     }
-  });
-  modalActionsContainer.appendChild(confirmButton);
+  if(e.key == "Escape" && !clicked){
+    console.log("WOWO");
+    backdrop = document.createElement('div');
+    backdrop.classList.add('backdrop');
+    document.body.insertBefore(backdrop, demoContainer);
+  
+    modal = document.createElement('div');
+    modal.classList.add('modal');
+  
+    var modalHeading = document.createElement('h1');
+    modalHeading.textContent = 'You sure you want to be a chicke- I mean, are you sure you want to quit?';
+    modal.appendChild(modalHeading);
+  
+    var imag = document.createElement('img');
+    imag.src = "modal_chic.jpg";
+    imag.width = 200
+    modal.appendChild(imag);
+  
+    var modalActionsContainer = document.createElement('div');
+    // modalActionsContainer.classList.add('modal-actions');
+    modal.appendChild(modalActionsContainer);
+  
+    var cancelButton = document.createElement('button');
+    cancelButton.setAttribute('type', 'button');
+    cancelButton.classList.add('btn-cancel');
+    cancelButton.textContent = 'Confirm';
+    cancelButton.addEventListener('click', closeModal);
+    modalActionsContainer.appendChild(cancelButton);
+  
+    var confirmButton = document.createElement('button');
+    confirmButton.setAttribute('type', 'button');
+    confirmButton.classList.add('btn-confirm');
+    confirmButton.textContent = 'Quit';
+    confirmButton.addEventListener('click', function() {
+      closeModal();
+    });
+    modalActionsContainer.appendChild(confirmButton);
+  
+    document.body.insertBefore(modal, demoContainer);
+  
+      
+  }
+  clicked=!clicked;
 
-  document.body.insertBefore(modal, demoContainer);
-
-  editedQuote = quote;
   //   textEdit.value = quote;
 });
+
+document.addEventListener("keydown", function g(e){console.log(e.key);});
