3934b24e72eaedd1647d662a9087dd6a00008671
Ayahassan94 <ayah7386@gmail.com>
2021-01-15 14:49:32 +0200

    about_page

--- about/js/about.js
+++ about/js/about.js
@@ -1,7 +1,5 @@
 
 $(".names").animate({top: '0px' ,height: '600px', width:'600px'},5000,function(){
-    $(this).css('color','pink')
+    $(this).css('color','red')
  })
 
-var audio = new Audio ("audio/spaceinvaders1.mpeg")
-audio.play();
